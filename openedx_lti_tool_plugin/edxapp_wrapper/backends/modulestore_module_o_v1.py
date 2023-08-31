43d7fcc4b0a3e8c50e202ab41f89f24ef3d774be
alexjmpb <alexander.mendoza@edunext.co>
2023-08-30 22:25:01 -0500

    feat: adding unpublished unit filtering

--- openedx_lti_tool_plugin/edxapp_wrapper/backends/modulestore_module_o_v1.py
+++ openedx_lti_tool_plugin/edxapp_wrapper/backends/modulestore_module_o_v1.py
@@ -1,7 +1,13 @@
 """student module backend (olive v1)."""
+from xmodule.modulestore.django import modulestore  # type: ignore # pylint: disable=import-error
 from xmodule.modulestore.exceptions import ItemNotFoundError  # type: ignore # pylint: disable=import-error
 
 
 def item_not_found_error_backend():
     """Return ItemNotFoundError class."""
     return ItemNotFoundError
+
+
+def modulestore_backend():
+    """Return modulestore function."""
+    return modulestore
